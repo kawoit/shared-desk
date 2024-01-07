b89ccbed8bd76c88725abff9c23fec2c6fd5636c
Willy Xamountry <willy.xamountry@googlemail.com>
2024-01-07 17:39:13 +0100

    changed js return

--- server/static/AdminView.js
+++ server/static/AdminView.js
@@ -118,8 +118,6 @@ submitNewRoom.addEventListener("submit", (event) => {
     }
   }
 
-  console.log(JSON.stringify(room));
-
   const url = "/admin";
 
   fetch(url, {
@@ -128,17 +126,11 @@ submitNewRoom.addEventListener("submit", (event) => {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(room), // Sending the room object as JSON
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      console.log("Success:", data); // Process the response data
-    })
-    .catch((error) => {
-      console.error("Error:", error); // Handle errors
-    });
+  }).then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    } else {
+      console.log("success");
+    }
+  });
 });
